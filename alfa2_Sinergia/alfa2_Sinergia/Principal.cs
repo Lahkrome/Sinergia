a423503004712a6ad9ccb33c14e3b9ad90dcbeff
Mario Ibarra <mariolp13@gmail.com>
2012-10-26 00:58:28 -0300

    Funcionalidades de agregar empresa y buscar datos
    Funcionalidades totalmente completas, falta la validacion de ellas, pero
    funcionan

---
+++ alfa2_Sinergia/alfa2_Sinergia/Principal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace alfa2_Sinergia
+{
+    public partial class FormPrincipal : Form
+    {
+        public FormPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Acerca de Sinergia: \nDirección: Alameda 1315 Of 32\nFono: 6989029",
+                        "Acerca de:",MessageBoxButtons.OK, MessageBoxIcon.Question);
+
+        }
+
+        private void btnEmpresa_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FormEmpresaPrincipal abrir = new FormEmpresaPrincipal();
+            abrir.Show();
+
+        }
+
+        private void FormPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
